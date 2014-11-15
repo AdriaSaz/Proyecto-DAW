8dcff602e3dd72ba30abf64ff77de800143b2e5e
AdriaSaz <asaz.informatica@gmail.com>
2014-11-15 15:42:16 +0100

    desde casa

--- Juego ahorcado/CUI/Program.cs
+++ Juego ahorcado/CUI/Program.cs
@@ -14,7 +14,7 @@ namespace CUI
         static void Main(string[] args)
         {
             int secreto = Juego.SecretoDeLaVida();
-            Console.WriteLine(secreto);
+            Console.WriteLine(secreto+2);
             Console.ReadKey();
         }
     }
