bd29cc52d7d240c8abbf4b30045def4c7e3d6dff
AdriaSaz <asaz.informatica@gmail.com>
2014-11-19 18:03:19 +0100

    20141119fin

--- POO/Clases/Persona.cs
+++ POO/Clases/Persona.cs
@@ -8,6 +8,14 @@ namespace Clases
 {
     public class Persona
     {
+        public String Nombre;
+        public String Apellido;
+        public string Email;
+
+        public string Presentarse()
+        {
+            return "Hola, me llamo " + this.Nombre + " " + this.Apellido;
+        }
 
     }
 }
--- POO/TestPersona/Programa.cs
+++ POO/TestPersona/Programa.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Clases;
 
 namespace TestPersona
 {
@@ -10,6 +11,38 @@ namespace TestPersona
     {
         static void Main()
         {
+
+           // Pruebas();
+            TestPresentacion();
+            Console.ReadKey();
+        }
+
+        private static void Pruebas()
+        {
+            Persona p;
+            p = new Persona();
+
+            Persona p2;
+            p2 = p;
+            p.Nombre = "pepito";
+
+            Console.WriteLine(p.Nombre);
+            string presentacion = p.Presentarse();
+            Console.WriteLine(presentacion);
+        }
+
+        static void TestPresentacion()
+        {
+            Persona p1 = new Persona();
+            p1.Nombre = "Roberto";
+            p1.Apellido = "Perez";
+
+            Persona p2 = new Persona();
+            p2.Nombre = "Rodolfo";
+            p2.Apellido = "Gomez";
+
+            Console.WriteLine(p1.Presentarse());
+            Console.WriteLine(p2.Presentarse());
 
         }
     }
