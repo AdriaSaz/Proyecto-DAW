9b2a2d5ced3e8c1481051ab60c5da76fe187eaae
AdriaSaz <asaz.informatica@gmail.com>
2014-12-09 16:00:18 +0100

    nbn

---
+++ POO/Herencia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clases;
+
+namespace Herencia
+{
+    class Empleado
+    {
+        static void Main()
+        {
+            Persona p1 = new Persona;
+            p1.Nombre = "Adria";
+            p1.Apellido = "saz";
+        }
+    }
+}
